11ef10d407e5a3c463a7261dd9d1585ba18b83ba
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-11 03:39:57 +0700

    practice binary search

--- dict/binary_search/binary.js
+++ dict/binary_search/binary.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = binary_serach;
+
 function binary_serach(arr, needle) {
     let lo = 0;
     let hi = arr.length;
@@ -9,19 +10,19 @@ function binary_serach(arr, needle) {
     do {
         let m = Math.floor(lo + (hi - lo) / 2);
         let v = arr[m];
+        console.log(`Checking value at index ${m}: ${v}`);
         if (v === n) {
             return true;
         } else if (v > n) {
             hi = m;
-            console.log(hi);
+            console.log(`New hi is ${hi}`);
         } else if (v < n) {
             lo = m + 1;
-            console.log(lo);
+            console.log(`New lo is ${lo}`);
         }
     } while (lo < hi);
     return false;
 }
 
 const number = [1, 2, 3, 4, 5];
-// console.log(binary_serach(number, 1));
-console.log(binary_serach(number, 3));
+console.log(binary_serach(number, 1)); // ค่าผลลัพธ์จะเป็น true
